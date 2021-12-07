c588e5552c59e6b671083c45f809eb2e79c725cd
june.park <laurenceyoon@gmail.com>
2021-12-08 01:47:51 +0900

    update whitenoise setting

--- app/settings.py
+++ app/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
